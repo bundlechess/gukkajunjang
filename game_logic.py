fb697bbfb376e9dbfec698f63cf29ea23d664594
bundlechess <2410910@jeohyeon.hs.kr>
2025-11-11 23:18:31 +0900

    Update game_logic.py

--- game_logic.py
+++ game_logic.py
@@ -8,16 +8,16 @@ class Game:
         self.players = {'ally': Player('ally'), 'enemy': Player('enemy')}
         self.turn = 0
 
-    def collect_gold(self, player_name):
+    def collect_gold(self, player_name: str):
         for tile in self.map.tiles.values():
-            if tile.terrain=='gold' and tile.owner==player_name and tile.gold_cooldown<=0:
+            if tile.terrain == 'gold' and tile.owner == player_name and tile.gold_cooldown <= 0:
                 amount = tile.gold_amount
                 self.players[player_name].money += amount
-                tile.gold_amount = random.randint(50,2000)
+                tile.gold_amount = random.randint(50, 2000)
                 tile.gold_cooldown = 12
                 print(f"{player_name} collected {amount} gold!")
 
     def update_cooldowns(self):
         for tile in self.map.tiles.values():
-            if tile.gold_cooldown>0:
+            if tile.gold_cooldown > 0:
                 tile.gold_cooldown -= 1
